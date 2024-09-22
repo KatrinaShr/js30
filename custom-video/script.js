522e152a8deab138c1bfb488d941fd9345d833d4
KatrinaShr <katrineshr@gmail.com>
2024-09-22 17:39:45 +0300

    feat: add progress-video-bar

--- custom-video/script.js
+++ custom-video/script.js
@@ -4,88 +4,109 @@ const additionalLightColor = rootVariablesCss.getPropertyValue('--additional-lig
 
 const playerEl = document.querySelector('.player');
 const videoEl = document.querySelector('.viewer');
-// const progress = playerEl.querySelector('.progress');
-// const progressBarEl = playerEl.querySelector('.progress__filled');
 
 const btnVideoPlayEl = playerEl.querySelector('.video-play');
 const btnPlayEl = playerEl.querySelector('.btn-play');
 const btnPauseEl = playerEl.querySelector('.btn-pause');
 const btnVolumeEl = playerEl.querySelector('.btn-volume');
+const btnMuteEl = playerEl.querySelector('.btn-mute');
 
 const progressVideoEl = document.querySelector('.progress-video');
 const progressSpeedEl = document.querySelector('.progress-speed');
 const progressVolumeEl = document.querySelector('.progress-volume');
 
 const titleSpeedEl = document.querySelector('.title-speed');
+let currentVideoDuration = 0;
 
-const video = {
-  isMute: false,
-  currentTime: 0,
-};
+document.addEventListener("DOMContentLoaded", init());
 
-function togglePlay() {
-  if (videoEl.paused) {
-    videoEl.play();
-    btnVideoPlayEl.classList.add('hidden');
-    btnPlayEl.classList.add('hidden');
+function init() {
+  videoEl.volume = 0.5;
+  progressVolumeEl.value = videoEl.volume * 100;
 
-    btnPauseEl.classList.remove('hidden');
-  } else {
-    videoEl.pause();
-    btnVideoPlayEl.classList.remove('hidden');
-    btnPlayEl.classList.remove('hidden');
+  updateProgressBar(progressSpeedEl);
+  updateProgressBar(progressVolumeEl);
+  updateProgressBar(progressVideoEl);
 
-    btnPauseEl.classList.add('hidden');
-  }
+  videoEl.addEventListener('loadedmetadata', () => {
+    currentVideoDuration = videoEl.duration;
+  });
 }
 
-videoEl.addEventListener('click', () => {
-  togglePlay();
-});
+function togglePlay() {
+  videoEl.paused ? playVideo() : stopVideo();
+}
 
-btnVideoPlayEl.addEventListener('click', () => {
-  togglePlay();
-});
+function playVideo() {
+  videoEl.play();
+  btnVideoPlayEl.classList.add('hidden');
+  btnPlayEl.classList.add('hidden');
+  btnPauseEl.classList.remove('hidden');
+}
 
-btnPlayEl.addEventListener('click', () => {
-  togglePlay();
-});
+function stopVideo() {
+  videoEl.pause();
+  btnVideoPlayEl.classList.remove('hidden');
+  btnPlayEl.classList.remove('hidden');
+  btnPauseEl.classList.add('hidden');
+}
 
-btnPauseEl.addEventListener('click', () => {
-  togglePlay();
-});
-  
+videoEl.addEventListener('click', togglePlay);
+btnVideoPlayEl.addEventListener('click', togglePlay);
+btnPlayEl.addEventListener('click', togglePlay);
+btnPauseEl.addEventListener('click', togglePlay);
+
 btnVolumeEl.addEventListener('click', () => {
-  video.isMute ? video.isMute = false : video.isMute = true;
+  btnVolumeEl.classList.add('hidden');
+  btnMuteEl.classList.remove('hidden');
+  videoEl.volume = 0;
+  progressVolumeEl.value = 0;
+  updateProgressBar(progressVolumeEl);
 });
 
-progressVideoEl.addEventListener('input', function() {
-  updateProgressBar(this);
+btnMuteEl.addEventListener('click', () => {
+  btnVolumeEl.classList.remove('hidden');
+  btnMuteEl.classList.add('hidden');
+  videoEl.volume = 0.5;
+  progressVolumeEl.value = 100;
+  updateProgressBar(progressVolumeEl);
 });
 
 progressSpeedEl.addEventListener('input', function() {
+  videoEl.playbackRate = this.value;
   updateProgressBar(this);
   titleSpeedEl.innerHTML = `${this.value}x`;
 });
 
 progressVolumeEl.addEventListener('input', function() {
+  videoEl.volume = this.value / 100;
   updateProgressBar(this);
 });
+
+progressVideoEl.addEventListener('input', function() {
+  const time = (this.value / 100) * currentVideoDuration;
+  videoEl.currentTime = time;
+});
+
+videoEl.addEventListener('timeupdate', updateProgressVideo);
+videoEl.addEventListener('ended', () => {
+  btnVideoPlayEl.classList.remove('hidden');
+  btnPlayEl.classList.remove('hidden');
+  btnPauseEl.classList.add('hidden');
+});
+
+function updateProgressVideo() {
+  if (videoEl.currentTime > 0) {
+    const value = Math.floor((videoEl.currentTime / videoEl.duration) * 100);
+
+    progressVideoEl.value = value;
+    progressVideoEl.style.background = `linear-gradient(to right, ${accentColorLight} 0%, ${accentColorLight} ${value}%, ${additionalLightColor} ${value}%, ${additionalLightColor} 100%)`;
+  }
+}
 
 function updateProgressBar(obj) {
   const value = obj.value;
   const max = obj.max || 100;
-  const percentage = (value / max) * 100;
-  obj.style.background = `linear-gradient(to right, ${accentColorLight} 0%, ${accentColorLight} ${percentage}%, ${additionalLightColor} ${percentage}%, ${additionalLightColor} 100%)`;
+  const percent = (value / max) * 100;
+  obj.style.background = `linear-gradient(to right, ${accentColorLight} 0%, ${accentColorLight} ${percent}%, ${additionalLightColor} ${percent}%, ${additionalLightColor} 100%)`;
 }
-
-// function updateBtnVolume(obj) {
-//   const value = obj.value;
-//   obj.style.background = `linear-gradient(to right, ${accentColorLight} 0%, ${accentColorLight} ${value}%, ${additionalLightColor} ${value}%, ${additionalLightColor} 100%)`;
-// }
-
-document.addEventListener("DOMContentLoaded", function() {
-  updateProgressBar(progressVideoEl);
-  updateProgressBar(progressSpeedEl);
-  updateProgressBar(progressVolumeEl);
-});
